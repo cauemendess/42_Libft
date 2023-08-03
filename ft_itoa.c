89a5b90f244dd96e3ff12427133d81672dc483d3
Cauê Mendes <caue_1456mendes@outlook.com>
2023-08-03 17:12:55 -0300

    itoa and striteri

---
+++ ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 14:11:31 by csilva-m          #+#    #+#             */
+/*   Updated: 2023/08/03 17:07:18 by csilva-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+static int	count_digits(int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb <= 0)
+		i++;
+	while (nb)
+	{
+		i++;
+		nb /= 10;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*result;
+	int		len;
+	long	num;
+
+	num = n;
+	len = count_digits(n);
+	result = (char *)malloc((len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	if (num < 0)
+	{
+		result[0] = '-';
+		num = num * -1;
+	}
+	if (num == 0)
+	{
+		result[0] = '0';
+	}
+	result[len--] = '\0';
+	while (num)
+	{
+		result[len] = (num % 10) + '0';
+		num = num / 10;
+		len--;
+	}
+	return (result);
+}
