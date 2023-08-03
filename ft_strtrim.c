33787f0a6a5bda7b3f214e236d22c621c2d03d51
Cauê Mendes <caue_1456mendes@outlook.com>
2023-08-03 11:18:54 -0300

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 09:46:23 by csilva-m          #+#    #+#             */
+/*   Updated: 2023/08/03 11:17:12 by csilva-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_char_set(char c, char const *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+
+	start = 0;
+	while (s1[start] && ft_char_set(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && ft_char_set(s1[end - 1], set))
+		end--;
+	return (ft_substr(s1, start, end - start));
+}
