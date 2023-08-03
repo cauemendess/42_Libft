85163e2376f04af62a7435da354e34998cf5ce9f
Cauê Mendes <caue_1456mendes@outlook.com>
2023-08-03 11:55:46 -0300

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 11:29:11 by csilva-m          #+#    #+#             */
+/*   Updated: 2023/08/03 11:51:40 by csilva-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned long	l;
+	unsigned long	i;
+
+	l = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (l + 1));
+	if (str == 0)
+		return (str);
+	i = 0;
+	while (i < l)
+	{
+		str[i] = f(i, str[i]);
+		++i;
+	}
+	str[i] = '\0';
+	return (str);
+}
